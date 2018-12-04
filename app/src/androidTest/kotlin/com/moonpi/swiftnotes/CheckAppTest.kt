36a939b6b72f8cf347923e4517c1a937dbc88dbe
MariaOskar <mariaoskarr@gmail.com>
2018-12-04 18:25:19 +0300

    Добавлены скриншоты и шаги, @Description заменен на @DisplayName, вынесены константы, добавлено удаление кеша и данных аллюра с устройства

--- app/src/androidTest/kotlin/com/moonpi/swiftnotes/CheckAppTest.kt
+++ app/src/androidTest/kotlin/com/moonpi/swiftnotes/CheckAppTest.kt
@@ -1,34 +1,39 @@
 package com.moonpi.swiftnotes
 
+import android.os.Environment
+import android.support.test.InstrumentationRegistry
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import ru.tinkoff.allure.annotations.Description
+import ru.tinkoff.allure.annotations.DisplayName
+import java.io.File
 
+const val TITLE = "Swiftnotes"
+const val NO_NOTES_DESCRIPTION = "Press '+' to add new note"
+const val NOTE_TITLE_FIELD_TEXT = "Title"
+const val NOTE_BODY_FIELD_TEXT = "Note"
+const val DIALOG_TEXT = "Save changes?"
+const val NOTE_TITLE = "Заметка 1"
+const val NOTE_BODY = "Тестовая запись 1"
+const val HIDE_NOTE_TEXT = "Hide note body in list"
+const val NOTE_FONT_SIZE_TEXT = "Note font size"
+const val RATE_APP_TEXT = "Rate app"
+const val RESTORE_NOTES_TEXT = "Restore notes"
+const val BACKUP_NOTES_TEXT = "Backup notes"
 
 @RunWith(AndroidJUnit4::class)
 class CheckAppTest{
-    private val TITLE = "Swiftnotes"
-    private val NO_NOTES_DESCRIPTION = "Press '+' to add new note"
-    private val NOTE_TITLE_FIELD_TEXT = "Title"
-    private val NOTE_BODY_FIELD_TEXT = "Note"
-    private val DIALOG_TEXT = "Save changes?"
-    private val NOTE_TITLE = "Заметка 1"
-    private val NOTE_BODY = "Тестовая запись 1"
-    private val HIDE_NOTE_TEXT = "Hide note body in list"
-    private val NOTE_FONT_SIZE_TEXT = "Note font size"
-    private val RATE_APP_TEXT = "Rate app"
-    private val RESTORE_NOTES_TEXT = "Restore notes"
-    private val BACKUP_NOTES_TEXT = "Backup notes"
 
     @get:Rule
     var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
     var app = SwiftNotesApplication();
 
     @Test()
-    @Description("Проверка состава активити и наличия диалогового окна при выходе с формы редактирования заметки")
+    @DisplayName("Проверка состава активити и наличия диалогового окна при выходе с формы редактирования заметки")
     fun checkAppTest() {
         app
             .checkApplicationTitle(TITLE)
@@ -45,7 +50,7 @@ class CheckAppTest{
 
 
     @Test
-    @Description("Проверка создания заметки и её наличия в списке заметок после сохранения")
+    @DisplayName("Проверка создания заметки и её наличия в списке заметок после сохранения")
     fun createNoteTest() {
         app
             .clickOnNewNoteButton()
@@ -58,7 +63,7 @@ class CheckAppTest{
     }
 
     @Test
-    @Description("Проверка содержимого меню на начальном экране и при редактировании заметки")
+    @DisplayName("Проверка содержимого меню на начальном экране и при редактировании заметки")
     fun checkMenuTest() {
         app
             .pressMoreButton()
@@ -70,5 +75,27 @@ class CheckAppTest{
             .pressMoreButton()
             .checkMenuItem(NOTE_FONT_SIZE_TEXT)
             .checkMenuItem(HIDE_NOTE_TEXT)
+    }
+
+    companion object {
+        val ALLURE_PATH = Environment.getExternalStorageDirectory().absolutePath + "/allure-results"
+        val CONTEXT_CACHE_PATH = InstrumentationRegistry.getContext().cacheDir.absolutePath
+        val TARGET_CONTEXT_CACHE_PATH = InstrumentationRegistry.getTargetContext().cacheDir.absolutePath
+
+        @BeforeClass
+        @JvmStatic
+        fun prepare() {
+            listOf(
+                ALLURE_PATH,
+                CONTEXT_CACHE_PATH,
+                TARGET_CONTEXT_CACHE_PATH
+            ).forEach { path -> clearDirectory(path) }
+        }
+
+        fun clearDirectory(path:String){
+            val directory = File(path)
+            if (directory.exists())
+                directory.deleteRecursively()
+        }
     }
 }
--- app/src/androidTest/kotlin/com/moonpi/swiftnotes/SwiftNotesApplication.kt
+++ app/src/androidTest/kotlin/com/moonpi/swiftnotes/SwiftNotesApplication.kt
@@ -9,117 +9,173 @@ import com.moonpi.swiftnotes.util.getDevice
 import org.hamcrest.CoreMatchers.allOf
 import org.hamcrest.CoreMatchers.containsString
 import org.hamcrest.Matchers
+import ru.tinkoff.allure.android.deviceScreenshot
+import ru.tinkoff.allure.step
+
+const val MORE_BUTTON_DESCRIPTION = "Ещё"
 
 class SwiftNotesApplication {
-    private val MORE_BUTTON_DESCRIPTION = "Ещё"
 
     fun checkApplicationTitle(text: String): SwiftNotesApplication {
-        onView(withText(text)).check(matches(isDisplayed()))
+        step("Проверяем заголовок приложения на соответствие строке: $text") {
+            onView(withText(text)).check(matches(isDisplayed()))
+        }
+        deviceScreenshot("checkApplicationTitle_"+text.hashCode())
         return this
     }
 
     fun checkNoNotesPlaceholder(text: String): SwiftNotesApplication {
-        onView(withId(R.id.noNotes)).check(matches(isDisplayed()))
-        onView(withId(R.id.noNotes)).check(matches(withText(text)))
+        step("Проверяем сообщение об отсутствии заметок на наличие и на соответствие строке: $text") {
+            onView(withId(R.id.noNotes)).check(matches(isDisplayed()))
+            onView(withId(R.id.noNotes)).check(matches(withText(text)))
+        }
+        deviceScreenshot("checkNoNotesPlaceholder_"+text.hashCode())
         return this
     }
 
     fun clickOnNewNoteButton(): SwiftNotesApplication {
-        onView(withId(R.id.newNote)).check(matches(isDisplayed()))
-        onView(withId(R.id.newNote)).perform(click())
+        step("Нажимаем на кнопку(\"+\") для создания новой Заметки") {
+            onView(withId(R.id.newNote)).check(matches(isDisplayed()))
+            onView(withId(R.id.newNote)).perform(click())
+        }
+        deviceScreenshot("clickOnNewNoteButton")
         return this
     }
 
     fun checkNoteTitlePlaceholder(text: String): SwiftNotesApplication {
-        onView(withId(R.id.titleEdit)).check(matches(withHint(text)))
+        step("Проверяем подсказку в поле для ввода название заметки на соответствие строке: $text") {
+            onView(withId(R.id.titleEdit)).check(matches(withHint(text)))
+        }
+        deviceScreenshot("checkNoteTitlePlaceholder_"+text.hashCode())
         return this
     }
 
     fun checkNoteBodyPlaceholder(text: String): SwiftNotesApplication {
-        onView(withId(R.id.bodyEdit)).check(matches(withHint(text)))
+        step("Проверяем подсказку в поле для ввода содержания заметки на соответствие строке: $text") {
+            onView(withId(R.id.bodyEdit)).check(matches(withHint(text)))
+        }
+        deviceScreenshot("checkNoteBodyPlaceholder_"+text.hashCode())
         return this
     }
 
     fun clickOnBackButton(): SwiftNotesApplication {
-        onView(
-            allOf(
-                withClassName(containsString("ImageButton")),
-                withParent(withId(R.id.toolbarEdit))
-            )
-        ).perform(click())
+        step("Нажимаем на кнопку \"<\"(Назад)") {
+            onView(
+                allOf(
+                    withClassName(containsString("ImageButton")),
+                    withParent(withId(R.id.toolbarEdit))
+                )
+            ).perform(click())
+        }
+        deviceScreenshot("clickOnBackButton")
         return this
     }
 
     fun checkDialogMessage(text: String): SwiftNotesApplication {
-        onView(withId(android.R.id.content)).check(matches(isDisplayed()))
-        onView(withId(android.R.id.message)).check(matches(withText(text)))
+        step("Проверяем текст диалогового сообщения на соответствие: $text") {
+            onView(withId(android.R.id.content)).check(matches(isDisplayed()))
+            onView(withId(android.R.id.message)).check(matches(withText(text)))
+        }
+        deviceScreenshot("checkDialogMessage_"+text.hashCode())
         return this
     }
 
     fun checkAcceptButtonOnDialog(): SwiftNotesApplication {
-        onView(withId(android.R.id.button1)).check(matches(isDisplayed()))
+        step("Проверяем наличие кнопки \"YES\"(Принять) в диалоговом окне") {
+            onView(withId(android.R.id.button1)).check(matches(isDisplayed()))
+        }
+        deviceScreenshot("checkAcceptButtonOnDialog")
         return this
     }
 
     fun checkCancelButtonOnDialog(): SwiftNotesApplication {
-        onView(withId(android.R.id.button2)).check(matches(isDisplayed()))
+        step("Проверяем наличие кнопки \"NO\"(Отклонить) в диалоговом окне") {
+            onView(withId(android.R.id.button2)).check(matches(isDisplayed()))
+        }
+        deviceScreenshot("checkCancelButtonOnDialog")
         return this
     }
 
     fun pressAcceptButtonOnDialog(): SwiftNotesApplication {
-        onView(withId(android.R.id.button1)).perform(click())
+        step("Нажимаем на кнопку \"YES\"(Принять)") {
+            onView(withId(android.R.id.button1)).perform(click())
+        }
+        deviceScreenshot("pressAcceptButtonOnDialog")
         return this
     }
 
     fun pressCancelButtonOnDialog(): SwiftNotesApplication {
-        onView(withId(android.R.id.button2)).perform(click())
+        step("Нажимаем на кнопку \"NO\"(Отклонить)") {
+            onView(withId(android.R.id.button2)).perform(click())
+        }
+        deviceScreenshot("pressCancelButtonOnDialog")
         return this
     }
 
 
     fun typeNoteTitle(text:String): SwiftNotesApplication {
-        onView(withId(R.id.titleEdit))
-            .perform(ViewActions.clearText())
-            .perform(ViewActions.replaceText(text))
+        step("Вводим текст \"$text\" в поле для ввода названия заметки") {
+            onView(withId(R.id.titleEdit))
+                .perform(ViewActions.clearText())
+                .perform(ViewActions.replaceText(text))
+        }
+        deviceScreenshot("typeNoteTitle_"+text.hashCode())
         return this
     }
 
     fun typeNoteBody(text:String): SwiftNotesApplication {
-        onView(withId(R.id.bodyEdit))
-            .perform(ViewActions.clearText())
-            .perform(ViewActions.replaceText(text))
+        step("Вводим текст \"$text\" в поле для ввода содержимого заметки") {
+            onView(withId(R.id.bodyEdit))
+                .perform(ViewActions.clearText())
+                .perform(ViewActions.replaceText(text))
+        }
+        deviceScreenshot("typeNoteBody_"+text.hashCode())
         return this
     }
 
     fun checkNoteWithTitleAndBody(title:String, body:String): SwiftNotesApplication {
-        onView(Matchers.allOf(withId(R.id.titleView), withText(title)))
-        onView(Matchers.allOf(withId(R.id.bodyView), withText(body)))
+        step("Проверяем наличие заметки с заголовком \"$title\" и содержимым \"$body\"") {
+            onView(Matchers.allOf(withId(R.id.titleView), withText(title)))
+            onView(Matchers.allOf(withId(R.id.bodyView), withText(body)))
+        }
+        deviceScreenshot("checkNoteWithTitleAndBody_"+title.hashCode()+"_"+body.hashCode())
         return this
     }
 
     fun checkNotesCount(min:Int): SwiftNotesApplication {
-        onView(withId(R.id.listView)).check(matches(hasMinimumChildCount(min)))
+        step("Проверяем, что количество заметок в списке больше $min") {
+            onView(withId(R.id.listView)).check(matches(hasMinimumChildCount(min)))
+        }
+        deviceScreenshot("checkNotesCount_"+min)
         return this
     }
 
     fun pressMoreButton(): SwiftNotesApplication {
-        onView(withContentDescription(MORE_BUTTON_DESCRIPTION)).perform(click())
+        step("Нажимаем на кнопку \"ЕЩЁ\"") {
+            onView(withContentDescription(MORE_BUTTON_DESCRIPTION)).perform(click())
+        }
+        deviceScreenshot("pressMoreButton")
         return this
     }
 
     fun checkMenuItem(text:String): SwiftNotesApplication {
-        onView(
-            Matchers.allOf(
-                withParent(withParent(withId(R.id.content))),
-                withId(R.id.title),
-                withText(text)
-            )
-        ).check(matches(isDisplayed()))
+        step("Проверяем наличие пункта меню \"$text\"") {
+            onView(
+                Matchers.allOf(
+                    withParent(withParent(withId(R.id.content))),
+                    withId(R.id.title),
+                    withText(text)
+                )
+            ).check(matches(isDisplayed()))
+            deviceScreenshot("checkMenuItem_"+text.hashCode())
+        }
         return this
     }
 
     fun pressBackButtonOnDevice(): SwiftNotesApplication {
-        getDevice().pressBack()
+        step("Нажимаем на кнопку Назад на устройстве") {
+            getDevice().pressBack()
+        }
         return this
     }
 }
